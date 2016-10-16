2f69672868aea89665bebad03b26f36abb2a1992
LMnet <lmnet89@gmail.com>
2016-10-16 18:41:55 +0700

    Refactor

--- src/main/scala/intro/DynamicExample.scala
+++ src/main/scala/intro/DynamicExample.scala
@@ -9,18 +9,22 @@ class DynamicExample {
 
     val d3 = js.Dynamic.global.d3
 
-    var data = js.Array(4, 8, 15, 16, 23, 42)
+    val data = js.Array(4, 8, 15, 16, 23, 42)
 
-    var x = d3.scale.linear()
+    val x = d3.scale.linear()
       .domain(js.Array(0, d3.max(data)))
       .range(js.Array(0, 420))
+
+    val styleCb = (d: js.Dynamic) => x(d) + "px"
+
+    val textCb = (d: js.Dynamic) => d
 
     d3.select(".chart")
       .selectAll("div")
       .data(data)
       .enter().append("div")
-      .style("width", (d: js.Dynamic) => x(d) + "px")
-      .text((d: js.Dynamic) => d)
+      .style("width", styleCb)
+      .text(textCb)
     //.ololo()
   }
 }
--- src/main/scala/intro/StaticExample.scala
+++ src/main/scala/intro/StaticExample.scala
@@ -15,18 +15,20 @@ class StaticExample {
       .domain(js.Array(0, d3.max(data)))
       .range(js.Array(0, 420))
 
-    val styleFunc: (js.Any, Double) => js.Any = {
+    val styleCb: (js.Any, Double) => js.Any = {
       (d: js.Any, _: Double) => {
         x(d) + "px"
       }
     }
 
+    val textCb = (d: js.Any) => d
+
     d3.select(".chart")
       .selectAll("div")
       .data(data)
       .enter().append("div")
-      .style("width", styleFunc)
-      .text((d: js.Any) => d)
+      .style("width", styleCb)
+      .text(textCb)
     //.ololo()
   }
 
